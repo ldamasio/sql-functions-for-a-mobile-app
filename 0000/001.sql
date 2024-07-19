929b6f97cf924469b5c1279101f2ca88a567be37
Leandro Damasio <ldamasio@gmail.com>
2024-07-19 17:00:37 -0300

    .

--- 0000/001.sql
+++ 0000/001.sql
@@ -54,4 +54,53 @@ $BODY$
 ALTER FUNCTION sigilomobile.g_retorna_saldo_contas_na_data(integer, date)
   OWNER TO moduladb;
 
+---
 
+Obs: a abordagem do get_hierarchy_level(pc.id) >= (get_hierarchy_level(sd.id) - 1)
+apenas substrai 1 da conta analítica, por isso a função estava retornando apenas 1 
+nível da conta analítica.
+
+    WITH
+    saldos AS (
+        SELECT DISTINCT ON (sd.cod_empresa, converte_id_para_comparar(sd.id))
+            sd.cod_empresa,
+            sd.id,
+            sd.cod_conta,
+            sd.nome_conta,
+            sd.redutora,
+            sd.permite_lancar,
+            CASE WHEN sd.redutora THEN -sd.saldo_final ELSE sd.saldo_final END AS saldo_conta
+        FROM
+            sigilomobile.retorna_saldo_e_movimentacao_contas(
+                1, 
+                '1.1.1.1.,1.1.1.2.,1.1.1.3.,1.1.1.4.,1.1.2.,2.1.', 
+                '2024-04-30', 
+                '2024-04-30'
+            ) sd
+        ORDER BY
+            sd.cod_empresa,
+            converte_id_para_comparar(sd.id)
+    )
+    SELECT
+        sd.cod_empresa,
+        pc.id::text,
+        pc.nome,
+        SUM(sd.saldo_conta) AS saldo_conta,
+        NULL::integer AS credibilidade
+    FROM
+        plano_contas pc
+        INNER JOIN saldos sd
+            ON (sd.id ILIKE pc.id || '%')
+    WHERE
+	pc.id ILIKE '1.1.1.1.%' OR 
+	pc.id ILIKE '1.1.1.2.%' OR 
+	pc.id ILIKE '1.1.1.3.%' OR
+	pc.id ILIKE '1.1.1.4.%' OR
+	pc.id ILIKE '1.1.2.%' OR
+	pc.id ILIKE '2.1.%'
+    GROUP BY
+        sd.cod_empresa,
+        pc.id,
+        pc.nome
+    ORDER BY
+        pc.id;
